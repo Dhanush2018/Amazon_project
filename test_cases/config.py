d3528a9bc0b7003e666d1c20730912d6068c58ec
Dhanush2018 <dhanushvk2000@gmail.com>
2024-07-10 16:02:08 +0530

    Updated

--- test_cases/config.py
+++
@@ -1,9 +0,0 @@
-URL = "https://www.amazon.in/"
-USER_EMAIL = "your email ID"
-USER_PASSWORD = "your password"
-
-INVALID_PASSWORD = "1234ABCD"
-
-PRODUCT = "Iphone 15 pro max"
-
-PRODUCTS = ["Iphone 15", "Samsung S24", "redmi note 13 pro"]
